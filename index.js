aaab83991d589b5ffa849f6b9642ff415c19abe0
codynetty <codynetty@gmail.com>
2023-12-04 19:07:52 -0700

    adding a path

--- index.js
+++ index.js
@@ -2,29 +2,7 @@ const express = require("express");
 
 let app = express();
 
-let path = require("path");
+app.get("/",( req, res) => res.send("This i the server running"))
 
-const port = 3000;
+app.listen(3000, () => console.log("im watching youo"));
 
-app.set("view engine", "ejs");
-
-app.use(express.urlencoded({extended : true}));
-
-const knex = require("knex")({
-    client: "pg",
-    connection: {
-        host: "localhost",
-        user: "postgres",
-        password: "packers20",
-        database: "bucket_list",
-        port: 5432
-    }
-})
-
-app.get("/", (req, res)=> {
-    knex.select().from("country").then( country  => {
-        res.render("index.html", { mycountry : country});
-    });
-});
-
-app.listen( port, () => console.log("myTravels is listening"));
